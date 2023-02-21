ee9bdb6d91a7a11c923cc52bbe20b831e909ae02
Kilian Agboton <kilian.agboton@gmail.com>
2023-02-21 02:07:59 -0500

    spring context

--- src/main/java/com/kagboton/spring6di/Spring6DiApplication.java
+++ src/main/java/com/kagboton/spring6di/Spring6DiApplication.java
@@ -1,13 +1,19 @@
 package com.kagboton.spring6di;
 
+import com.kagboton.spring6di.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring6DiApplication.class, args);
+        ApplicationContext context = SpringApplication.run(Spring6DiApplication.class, args);
+        MyController controller = context.getBean(MyController.class);
+
+        System.out.println("In Main method");
+        System.out.println(controller.sayHello());
     }
 
 }
---
+++ src/main/java/com/kagboton/spring6di/controllers/MyController.java
@@ -0,0 +1,12 @@
+package com.kagboton.spring6di.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello(){
+        System.out.println("In the controller");
+        return "Hello World!";
+    }
+}
